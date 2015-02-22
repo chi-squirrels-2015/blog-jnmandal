64d3905f8eda6103f9a8d80a24cc0d18b567d6f5
John N. Mandalakas <jnmandal@users.noreply.github.com>
2015-02-22 14:10:38 -0600

    add routes, controllers, and views for posts; rough working CRUD for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,45 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to "/posts/#{@post.id}"
+    else
+      render "new"
+    end
+  end
+
+  # how is there not a better way to do this?
+  def post_params
+    params.require(:post).permit(:title, :content, :publish_date)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to "/posts/#{@post.id}"
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/posts"
+  end
+
+end
